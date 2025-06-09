7178573a5dfaa7d016a951f978da62a6f87b51ef
MrGetRext <mrgetrext@hotmail.com>
2025-06-09 10:45:39 -0400

    Callbacks

--- src/ReplicatedStorage/Modules/Framework/RextFramework.lua
+++ src/ReplicatedStorage/Modules/Framework/RextFramework.lua
@@ -1,55 +1,109 @@
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+
 local Promise = require(game.ReplicatedStorage.Source.Modules.Packages.Promise)
-local Loader = require(game.ReplicatedStorage.Source.Modules.Packages.Loader)
-local Component = require(game.ReplicatedStorage.Source.Modules.Packages.Component)
+local Trove = require(game.ReplicatedStorage.Source.Modules.Packages.Trove)
 
 local RextFramework = {}
 
 RextFramework._loadedModules = {}
 RextFramework._loadedComponents = {}
+RextFramework._troves = {}
 
 function RextFramework:Start(modulesToLoad, modulePredicate, componentsToLoad, componentPredicate)
     return Promise.new(function(resolve, reject, onCancel)
-
-        -- #region Load Modules
-        for _, moduleFile in pairs(modulesToLoad) do 
-            if not moduleFile:IsA("ModuleScript") or (modulePredicate and not modulePredicate(moduleFile)) then
-                continue
-            end
-
-            print("Loading module " .. moduleFile.Name)
-            self._loadedModules[moduleFile.Name] = require(moduleFile)
-        end
-
-        for name, module in pairs(self._loadedModules) do
-            if module.Awake then
-                print(name.." ".." awake")
-                module:Awake()
-            end
-        end
-
-        for name, module in pairs(self._loadedModules) do
-            if module.Start then
-                print(name.." ".." start")
-                module:Start()
-            end
-        end
-        -- #endregion
-
-        -- #region Setup Additional Module Callbacks
-
-        -- #endregion
-
-        -- #region Load Components
-        for _, componentFile in pairs(componentsToLoad) do 
-            if not componentFile:IsA("ModuleScript") or (componentPredicate and not componentPredicate(componentFile)) then
-                continue
-            end
-
-            print("Loading component " .. componentFile.Name)
-            self._loadedComponents[componentFile.Name] = require(componentFile)
-        end
-        -- #endregion
+        self:_loadModules(modulesToLoad, modulePredicate)
+        self:_setupAdditionalModuleCallbacks()
+        self:_loadComponents(componentsToLoad, componentPredicate)
     end)
+end
+
+function RextFramework:_loadModules(modulesToLoad, modulePredicate)
+    for _, moduleFile in pairs(modulesToLoad) do 
+        if not moduleFile:IsA("ModuleScript") or (modulePredicate and not modulePredicate(moduleFile)) then
+            continue
+        end
+
+        print("Loading module " .. moduleFile.Name)
+        self._loadedModules[moduleFile.Name] = require(moduleFile)
+    end
+
+    for name, module in pairs(self._loadedModules) do
+        if module.Awake then
+            print(name.." ".." awake")
+            module:Awake()
+        end
+    end
+
+    for name, module in pairs(self._loadedModules) do
+        if module.Start then
+            print(name.." ".." start")
+            module:Start()
+        end
+    end
+end
+
+function RextFramework:_setupAdditionalModuleCallbacks()
+    local function notifyModules(functionName, ...)
+        for moduleName, module in pairs(RextFramework._loadedModules) do
+            local fn = module[functionName]
+            if typeof(fn) == "function" then
+                local ok, err = pcall(fn, ...)
+                if not ok then
+                    warn("[RextFramework] Module error in " .. moduleName .. "/" .. functionName .. ": " .. tostring(err))
+                end
+            end
+        end
+    end
+
+    local function onPlayerCharacterAdded(player: Player, character: Model)
+        notifyModules("OnPlayerCharacterAdded", player, character)
+    end
+
+    local function onPlayerAdded(player: Player)
+        notifyModules("OnPlayerAdded", player)
+
+        -- Init
+        if player.Character then
+            task.defer(onPlayerCharacterAdded, player, player.Character)
+        end
+
+        -- Listen
+        RextFramework._troves[player] = Trove.new()
+        RextFramework._troves[player]:Add(player.CharacterAdded:Connect(function(character: Model)
+            task.defer(onPlayerCharacterAdded, player, character)
+        end))
+    end
+
+    local function onPlayerRemoving(player: Player)
+        notifyModules("OnPlayerRemoving", player)
+        
+        -- Cleanup
+        if RextFramework._troves[player] then
+            RextFramework._troves[player]:Destroy()
+            RextFramework._troves[player] = nil
+        end
+    end
+
+    -- Init
+    for _, player in pairs(Players:GetPlayers()) do
+        task.spawn(onPlayerAdded, player)
+    end
+
+    -- Listen
+    Players.PlayerAdded:Connect(onPlayerAdded)
+    Players.PlayerRemoving:Connect(onPlayerRemoving)
+end
+
+function RextFramework:_loadComponents(componentsToLoad, componentPredicate)
+    for _, componentFile in pairs(componentsToLoad) do 
+        if not componentFile:IsA("ModuleScript") or (componentPredicate and not componentPredicate(componentFile)) then
+            continue
+        end
+
+        print("Loading component " .. componentFile.Name)
+        self._loadedComponents[componentFile.Name] = require(componentFile)
+    end
 end
 
 return RextFramework
