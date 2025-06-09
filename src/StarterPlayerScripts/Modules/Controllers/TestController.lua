870867f900711d1c359314edb2c80b3fac3f6950
MrGetRext <mrgetrext@hotmail.com>
2025-06-08 23:06:38 -0400

    test

--- src/StarterPlayerScripts/Modules/Controllers/TestController.lua
+++ src/StarterPlayerScripts/Modules/Controllers/TestController.lua
@@ -11,6 +11,8 @@ function TestController:Start()
 
     local data = Network.queries.GetPlayerData.invoke({ playerId = game.Players.LocalPlayer.UserId })
     print(data.level, data.xp)
+
+    Network.packets.Notify.send({ imporant = false, message = "Hello" })
 end
 
 return TestController
